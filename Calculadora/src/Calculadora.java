409c4021ed4191d33ebf17769b02527950df8b49
esterafalatin <stevraffs@gmail.com>
2014-07-18 08:19:10 -0600

    Revert "Cambio 1.0.5"
    This reverts commit 9610537602757e86eacd9997bda45e3776f30481.

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.io.*;
+
 /**
  *
  * @author Bicimotoca
@@ -13,24 +13,6 @@ public class Calculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try{
-            // Abrimos el archivo
-            FileInputStream fstream = new FileInputStream("LeerArchivo.java");
-            // Creamos el objeto de entrada
-            DataInputStream entrada = new DataInputStream(fstream);
-            // Creamos el Buffer de Lectura
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
-            String strLinea;
-            // Leer el archivo linea por linea
-            while ((strLinea = buffer.readLine()) != null)   {
-                // Imprimimos la línea por pantalla
-                System.out.println (strLinea);
-            }
-            // Cerramos el archivo
-            entrada.close();
-        }
-        catch (Exception e){ //Catch de excepciones
-            System.err.println("Ocurrio un error: " + e.getMessage());
-        }
+        // TODO code application logic here
     }
 }
