35be927f8559537cf1a1bf4ec4d72ab562842704
peilor <arg13028@uvg.edu.gt>
2014-07-18 07:25:03 -0600

    Cambio 1.0.0

---
+++ Programa/Ejemplo/StackEjemplo.java
@@ -0,0 +1,14 @@
+
+
+public class StackEjemplo {
+  public static void main(String[] args) {
+    Stack<String> miStack = new StackArrayList<String>();
+    miStack.push("Adios");  //1
+	miStack.push("Hola");   //2
+
+    String cadena = miStack.pop();
+    System.out.println(cadena);
+	cadena = miStack.pop();
+	System.out.println(cadena);
+  }
+}
