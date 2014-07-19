3efee88051bd5520ad13ea61b029e159be76e711
peilor <arg13028@uvg.edu.gt>
2014-07-19 16:27:41 -0600

    Cambio 1.0.6

--- Programa/Ejemplo/StackEjemplo.java
+++ Programa/Ejemplo/StackEjemplo.java
@@ -2,11 +2,16 @@
 
 public class StackEjemplo {
   public static void main(String[] args) {
-    Stack<String> miStack = new StackArrayList<String>();
+    Stack<String> miStack = new StackVector<String>();
     miStack.push("Adios");  //1
 	miStack.push("Hola");   //2
-
-    String cadena = miStack.pop();
+	
+	
+	System.out.println(miStack.size());
+	System.out.println(miStack.empty());
+	String cadena = miStack.peek();
+    System.out.println(cadena);
+    cadena = miStack.pop();
     System.out.println(cadena);
 	cadena = miStack.pop();
 	System.out.println(cadena);
