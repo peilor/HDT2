b09a673088a2029b3dd71716018225fa1bd0ec5a
AEGG <aegiron@hotmail.com>
2014-07-19 18:53:40 -0600

    Cambio 1.0.9

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,28 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-import java.io.*;
-/**
- *
- * @author Bicimotoca
- */
-public class Calculadora {
 
+
+import java.io.*;
+
+public class Calculadora {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Stack<String> calculos = new StackVector<String>();
+        Stack<Integer> datos = new StackVector<Integer>();
+      
         try{
             // Abrimos el archivo
-            FileInputStream fstream = new FileInputStream("LeerArchivo.java");
+            FileInputStream fstream = new FileInputStream("C:\\Users\\Acer\\Documents\\UVG\\4to semestre\\Alg Estructura Datos\\HDT2\\HDT2\\calculadora.txt");
             // Creamos el objeto de entrada
             DataInputStream entrada = new DataInputStream(fstream);
             // Creamos el Buffer de Lectura
             BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
             String strLinea;
+            int cont=0;
             // Leer el archivo linea por linea
             while ((strLinea = buffer.readLine()) != null)   {
+                calculos.push(strLinea);
                 // Imprimimos la línea por pantalla
                 System.out.println (strLinea);
             }
@@ -31,6 +30,6 @@ public class Calculadora {
         }
         catch (Exception e){ //Catch de excepciones
             System.err.println("Ocurrio un error: " + e.getMessage());
-        }
+        }        
     }
 }
