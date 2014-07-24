2c85b673a7d62bc9fda1ba75cf0d7ec094af01a1
peilor <arg13028@uvg.edu.gt>
2014-07-23 23:05:53 -0600

    Cambio 1.1.9

--- Programa/Calculadora.java
+++ Programa/Calculadora.java
@@ -1,32 +1,33 @@
+
+
 import java.io.*;
 
 public class Calculadora {
     private static Stack<Integer> datos;
-    public static String strLinea;
+    //public static String strLinea;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         datos= new StackVector<Integer>();
-        
-          
-        
+        String strLinea1=" ";
+        String strLinea= " ";		
         //Inicia codigo tomado de internet para la lectura de un archivo .txt
         try{
             // Abrimos el archivo
-            FileInputStream fstream = new FileInputStream("C:\\Users\\Acer\\Documents\\UVG\\4to semestre\\Alg Estructura Datos\\HDT2\\HDT2\\calculadora.txt");
+            FileInputStream fstream = new FileInputStream("texto.txt");
             // Creamos el objeto de entrada
             DataInputStream entrada = new DataInputStream(fstream);
             // Creamos el Buffer de Lectura
             BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
             
             // Leer el archivo linea por linea
-            while ((strLinea = buffer.readLine()) != null)   {
+            while ((strLinea1 = buffer.readLine()) != null)   {
+              
+                // Imprimimos la línea por pantalla
+				strLinea = strLinea1;
+                System.out.println ("Operacion ingresada: "+strLinea);
                 
-                
-                // Imprimimos la lÃ­nea por pantalla
-                System.out.println (strLinea);
-                operaciones();
             }
             // Cerramos el archivo
             entrada.close();
@@ -36,35 +37,23 @@ public class Calculadora {
         } 
         //Termina codigo tomado de internet
         
-        while (!datos.empty()){
-            System.out.println(datos.pop());
-            operaciones();
-        }
+       
         
-    }
-    
-    
-    private static void operaciones(){
        int resultado=0;
-       int num1 = datos.pop();
-       int num2 = datos.pop();
-       
-       
-       //System.out.println(dato);
+       int num1 = 0;
+       int num2 = 0;   
        for(int i=0; i<strLinea.length(); i++){
                 if(strLinea.charAt(i)=='+'){
                     num1 = datos.pop();
                     num2 = datos.pop();
                     resultado = num1 + num2;
                     datos.push(resultado);
-                    System.out.println (resultado);
                 }
                 if(strLinea.charAt(i)=='-'){
                     num1 = datos.pop();
                     num2 = datos.pop();
-                    resultado = num1 - num2;
+                    resultado = num2 - num1;
                     datos.push(resultado);
-                    System.out.println (strLinea);
                 }
                 if(strLinea.charAt(i)=='*'){
                     num1 = datos.pop();
@@ -75,21 +64,15 @@ public class Calculadora {
                 if(strLinea.charAt(i)=='/'){
                     num1 = datos.pop();
                     num2 = datos.pop();
-                    resultado = num1 / num2;
+                    resultado = num2 / num1;
                     datos.push(resultado);
                 }
-                 if(strLinea.charAt(i)==' '){
-                    i++;
-                }
-                
-                else
+                if (Character.getNumericValue(strLinea.charAt(i))>=0)
                 {
-                   int dato1=(int)strLinea.charAt(i);
-                   int dato2= Character.getNumericValue(dato1);
-                   datos.push(dato2);
-                }
-      
-                }
-                
-    }     
+				   int dato=Character.getNumericValue(strLinea.charAt(i));
+                   datos.push(dato);
+                }      
+        }
+       System.out.println ("El resultado de las operaciones es: "+datos.pop()); 
+    }
 }
